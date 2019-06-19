f84947a4beac93ab774dda8b896f238346df4b3e
Thomas Jakobi <thomas.jakobi@partout.info>
2019-06-19 09:21:59 +0200

    Add defaultredir and notloggedredir snippet properties

--- core/components/redirectusergroups/elements/snippets/redirectusergroups.snippet.php
+++ core/components/redirectusergroups/elements/snippets/redirectusergroups.snippet.php
@@ -1,49 +1,72 @@
+<?php
 /**
  * redirectUsergroups
  *
- * Copyright 2009-2011 by Bruno Perner <b.perner@gmx.de>
+ * Copyright 2009-2013 by Bruno Perner <b.perner@gmx.de>
  *
  * redirectUsergroups is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the Free
  * Software Foundation; either version 2 of the License, or (at your option) any
  * later version.
  *
- * redirectUsergroups is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * redirectUsergroups is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
  * You should have received a copy of the GNU General Public License along with
- * redirectUsergroups; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
- * Suite 330, Boston, MA 02111-1307 USA
+ * redirectUsergroups; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * @package redirectUsergroups
+ * @package redirectusergroups
+ * @subpackage snippet
  */
-/**
- * redirectUsergroups
- *
- * Redirect Users of specified groups to specified Resources
- *
- * Example:
- *
- * [[!redirectUsergroups? &redirs=`Frontenduser:3,Testuser:8`]]
- *
- */
+// Check parameters and set them to default values
+$redirs = $modx->getOption('redirs', $scriptProperties, '');
+$defaultredir = $modx->getOption('defaultredir', $scriptProperties, FALSE);
+$notloggedredir = $modx->getOption('notloggedredir', $scriptProperties, FALSE);
 
-if (isset($redirs)){
-    $redirs=explode(',',$redirs);
-    if (count($redirs)>0){
-        foreach ($redirs as $redir){
-      
-          $redir=explode(':',$redir);
-              
-            if (count($redir)==2){
-   
-                if ($modx->user->isMember($redir[0])){
-                    $url=$modx->makeUrl($redir[1]);
-                }            
-            }
-        }
-    }
+$url = '';
+$found = FALSE;
+
+if (!empty($redirs)) {
+	$redirs = explode(',', $redirs);
+	foreach ($redirs as $redir) {
+		// Get the current specified usergroup and resource id combination
+		$redir = explode(':', $redir);
+		if (count($redir) == 2) {
+			// Check if user is member of the current specified usergroup
+			if ($modx->user->isMember($redir[0])) {
+				$url = $modx->makeUrl($redir[1]);
+				$found = TRUE;
+				break;
+			}
+		}
+	}
 }
 
-return  $modx->sendRedirect($url);
+if (!$found && $defaultredir !== FALSE && $modx->user->get('id') !== 0) {
+	// The logged user is not in any specified usergroup
+	if (is_numeric($defaultredir)) {
+		// defaultredir is a resource id
+		$url = $modx->makeUrl($defaultredir);
+	} else {
+		// otherwise treat it as url
+		$url = $defaultredir;
+	}
+} elseif ($notloggedredir !== FALSE && $modx->user->get('id') === 0) {
+	// The user is not logged in
+	if (is_numeric($notloggedredir)) {
+		// defaultredir is a resource id
+		$url = $modx->makeUrl($notloggedredir);
+	} else {
+		// otherwise treat it as url
+		$url = $notloggedredir;
+	}
+}
+
+if (!empty($url)) {
+	$modx->sendRedirect($url);
+}
+return '';
+?>
